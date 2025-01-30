9e3f1f67c3e95b145aa72f05a320bf695e748733
Didas Mbalanya <didasmbalanya@gmail.com>
2025-01-30 20:11:16 +0300

    add pagination

--- src/functions/getOrder.ts
+++ src/functions/getOrder.ts
@@ -3,27 +3,40 @@ import { DynamoDB } from "aws-sdk";
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
-export const get: APIGatewayProxyHandler = async ({ pathParameters }) => {
+export const get: APIGatewayProxyHandler = async ({ pathParameters, queryStringParameters  }) => {
   const orderId = pathParameters?.orderId;
+  const queryParams = queryStringParameters || {};
+  const limit = queryParams.limit ? parseInt(queryParams.limit, 10) : 10; // Default page size
+  const lastEvaluatedKey = queryParams.lastEvaluatedKey
+    ? {orderId: queryParams.lastEvaluatedKey}
+    : undefined;
 
   if (!orderId) {
+
     const params = {
       TableName: 'OrdersTable',
+      Limit: limit,
+      ExclusiveStartKey: lastEvaluatedKey,
     };
+  
 
     try {
       const result = await dynamoDb.scan(params).promise();
       return {
         statusCode: 200,
-        body: JSON.stringify(result.Items),
+        body: JSON.stringify({
+          items: result.Items,
+          lastEvaluatedKey: result.LastEvaluatedKey,
+        }),
       };
     } catch (error) {
       return {
         statusCode: 500,
-        body: JSON.stringify({ message: 'Could not retrieve orders', error }),
+        body: JSON.stringify({ message: "Could not retrieve orders", error }),
       };
     }
   }
+
   const params = {
     TableName: "OrdersTable",
     Key: {
@@ -51,5 +64,3 @@ export const get: APIGatewayProxyHandler = async ({ pathParameters }) => {
     };
   }
 };
-
-
